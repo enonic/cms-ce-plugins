3aab771bab69ab323a8746af170c0323c927d0eb
Sten Roger Sandvik <srs@enonic.com>
2013-10-02 11:54:57 +0200

    Added href for attachments.

--- modules/cms-confluence-plugin/src/main/java/com/enonic/cms/plugin/confluence/xml/LinkRewriter.java
+++ modules/cms-confluence-plugin/src/main/java/com/enonic/cms/plugin/confluence/xml/LinkRewriter.java
@@ -10,7 +10,7 @@ public final class LinkRewriter
      */
     private final static String[] PATTERNS =
         {"href=\"/display/([^/]+)/([^\"]+)\"", "href='/display/([^/]+)/([^']+)'", "href=\"#([^\"]+)\"", "href='#([^']+)'",
-            "src=\"/download/attachments/([0-9]+)/([^\"]+)\"", "src='/download/attachments/([0-9]+)/([^']+)'", "src=\"/images/([^\"]+)\"",
+            "(src|href)=\"/download/attachments/([0-9]+)/([^\"]+)\"", "(src|href)='/download/attachments/([0-9]+)/([^']+)'", "src=\"/images/([^\"]+)\"",
             "src='/images/([^']+)'"};
 
     /**
@@ -18,7 +18,7 @@ public final class LinkRewriter
      */
     private final static String[] REPLACEMENTS =
         {"href=\":::page:$1:$2:::\"", "href=\":::page:$1:$2:::\"", "href=\":::anchor:$1:::\"", "href=\":::anchor:$1:::\"",
-            "src=\":::attachment:$1:$2:::\"", "src=\":::attachment:$1:$2:::\"", "src=\":::image:$1:::\"", "src=\":::image:$1:::\""};
+            "$1=\":::attachment:$2:$3:::\"", "$1=\":::attachment:$2:$3:::\"", "src=\":::image:$1:::\"", "src=\":::image:$1:::\""};
 
     /**
      * Rewrite links.
